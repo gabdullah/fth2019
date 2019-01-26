6085d80032d678abf767b46ee1ccf1b38c8c3c67
EFrankhouse <efrankho@kent.edu>
2019-01-26 14:20:06 -0500

    Added new routes for the new views

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Profile from './views/Profile.vue'
+import Login from './views/Login.vue'
+import Register from './views/Register.vue'
 
 Vue.use(Router)
 
@@ -10,16 +13,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
---
+++ src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Register</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'register',
+  components: {
+
+  }
+}
+</script>
