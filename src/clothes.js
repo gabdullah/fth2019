7d9f2d9c86e8d0c31f6e35d3f8bb98ea14ccb0f7
gabdullah <gabdull2@kent.edu>
2019-01-26 15:33:08 -0500

    starting clothes

--- src/clothes.js
+++ src/clothes.js
@@ -1,15 +1,44 @@
 export default [
     {
         name: 'Tops',
-        catagories: [
+        items: [
             {
                 name: 'Dress',
-                icon: 'dress.png'
+                icon: '@/assets/icons/dress.png'
             },
             {
                 name: 'Hoodie',
-                icon: 'hoodie.png',
-            }
+                icon: '@/assets/icons/hoodie.png',
+            },
+            {
+                name: 'Coat',
+                icon: '@/assets/icons/coat.png',
+            },
+            {
+                name: 'Jacket',
+                icon: '@/assets/icons/jacket.png',
+            },
+            {
+                name: 'Top',
+                icon: '@/assets/icons/t-shirt.png',
+            },
+            {
+                name: 'Polo',
+                icon: '@/assets/icons/polo.png',
+            },
+        ]
+    },
+    {
+        name: 'Bottoms',
+        items: [
+            {
+                name: 'Skirt',
+                icon: '@assets/icons/skirt.png',
+            },
+            {
+                name: '',
+                icon: '@assets/icons/skirt.png',
+            },
         ]
     }
 ]
